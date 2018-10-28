2a26a59bff9cf755da2340678000e14d075bcf68
Mariusz Borowiak <borowiakm85@gmail.com>
2018-10-28 18:22:40 +0000

    Simple int variables

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -4,5 +4,16 @@ public class Hello {
 
         System.out.println("Hello, World!");
 
+        int myFirstNumber = (10 + 5)*(2 + 2);
+        int mySecondNumber = 1;
+        int myThirdNumber = myFirstNumber + mySecondNumber*2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myThousandLessTotal = 1000 - myTotal;
+
+        System.out.println(myFirstNumber);
+        System.out.println("----");
+        System.out.println(myTotal);
+        System.out.println(myThousandLessTotal);
     }
 }

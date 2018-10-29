88a712b58d26ca0338fac875bab31f765fe491b5
Mariusz Borowiak <borowiakm85@gmail.com>
2018-10-29 13:17:39 +0000

    Method overloading basics

--- MethodOverloading/src/com/cthulh/Main.java
+++ MethodOverloading/src/com/cthulh/Main.java
@@ -3,6 +3,30 @@ package com.cthulh;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        calcFeetAndInchesToCentimeters(157);
+
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
+
+        double result = -1;
+
+        if (feet >= 0 && inches >= 0 && inches <= 12) {
+            result = (feet*12 + inches)*2.54;
+        }
+        System.out.println(result);
+        return result;
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double inches) {
+
+        double result = -1;
+
+        if (inches >= 0) {
+            result = calcFeetAndInchesToCentimeters((int)inches/12, inches%12);
+        }
+
+        return result;
     }
 }

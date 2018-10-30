e391e11f5a939ec2f2360e1f414f08ef70bd4943
Mariusz Borowiak <borowiakm85@gmail.com>
2018-10-30 09:55:28 +0000

    Add getDurationString method

--- SecondsAndMinutes/src/SecondsAndMinutes.java
+++ SecondsAndMinutes/src/SecondsAndMinutes.java
@@ -4,7 +4,22 @@ public class SecondsAndMinutes {
 
     }
 
-    public static int getMinutes(){
-
+    public static String getDurationString(int minutes, int seconds){
+        String errorMessage = "Invalid value";
+        if (minutes >= 0 && seconds >= 0 && seconds <= 59) {
+            String durationMessage = "";
+            int hours = minutes/60;
+            int reminderMinutes = minutes % 60;
+            String showHours = hours < 10? "0" + hours: "" + hours;
+            String showMinutes = reminderMinutes < 10? "0" + reminderMinutes: "" + reminderMinutes;
+            String showSeconds = seconds < 10? "0" + seconds: "" + seconds;
+            durationMessage += showHours + "h "
+                            + showMinutes + "m "
+                            + showSeconds + "s";
+            return durationMessage;
+        } else {
+            return errorMessage;
+        }
     }
+
 }
